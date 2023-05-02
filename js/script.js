4cb4aa8c46b5005dae508b09ac4bbad53a3f1eaa
Bonnie-Zhu <124389118+Bonnie-Zhu@users.noreply.github.com>
2023-05-02 16:59:09 +0000

    done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) Bonnie Zhu
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Bonnie Zhu
+// Created on: May 2023
 // This file contains the JS functions for index.html
+"use strict"
+
+function calculate() {
+  const age = document.getElementById("age").value
+
+  if (age >= 18) {
+    document.getElementById("answer").innerHTML =
+      "You can see G, PG and R rated movies!"
+  } else if (age >= 13) {
+    document.getElementById("answer").innerHTML =
+      "You can see G and PG rated movies!"
+  } else if (age >= 5) {
+    document.getElementById("answer").innerHTML = "You can see G rated movies!"
+  } else {
+    document.getElementById("answer").innerHTML =
+      "you can hardly watch any movies"
+  }
+  console.log("Thanks for verifying you're age!")
+}
