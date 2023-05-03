0265e45eb2bc0b2c9d3bb417e221f52fc2f0320d
Bonnie-Zhu <124389118+Bonnie-Zhu@users.noreply.github.com>
2023-05-03 00:01:50 +0000

    add main.go

---
+++ go_app/main.go
@@ -0,0 +1,30 @@
+//create by: Bonnie Zhu
+//create on: May 2023
+
+
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	var ageInput string
+	fmt.Print("Enter your age: ")
+	fmt.Scanln(&ageInput)
+
+	age, err := strconv.ParseFloat(ageInput, 64)
+	switch {
+	case err != nil:
+		fmt.Println("Invalid input, please enter a number.")
+	case age >= 17:
+		fmt.Println("You can see an R rated movie alone.")
+	case age >= 13:
+		fmt.Println("You can see a PG-13 movie alone.")
+	case age >= 6:
+		fmt.Println("You can see a G or PG movie alone.")
+	default:
+		fmt.Println("You're too young to see anything.")
+	}
+}
